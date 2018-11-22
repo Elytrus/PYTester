df119426046f8810fc82de1bf0daa8d9e48c52fc
Elytrus <moses@mosesxu.net>
2018-11-22 10:58:02 -0500

    fix gen

--- test/test.py
+++ test/test.py
@@ -11,14 +11,14 @@ tester = TextHandler()
 tester.test(open('iotest\\cases\\test_ir.ini'))  # IR Test
 
 tester.test(open('iotest\\cases\\test.ini'))  # Normal Test
-tester.test(open('iotest\\cases\\test_c.ini')) # C Test
-tester.test(open('iotest\\cases\\test_cpp.ini')) # CPP Test
+tester.test(open('iotest\\cases\\test_c.ini'))  # C Test
+tester.test(open('iotest\\cases\\test_cpp.ini'))  # CPP Test
 
 tester.test(open('iotest\\cases\\test_no_check_correct.ini'))  # No Correctness Test
 tester.test(open('iotest\\cases\\test_no_check_time.ini'))  # No Time Limit Test
 
 tester.test(open('iotest\\cases\\test_fast.ini'))  # WA Test
 
-tester.test(open('iotest\\cases\\test.ini'), True) # Async Test
+tester.test(open('iotest\\cases\\test.ini'), True)  # Async Test
 
 print('random message here')
